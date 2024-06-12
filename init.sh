c8eed9750f65bc3176aa84500ac9b77a86142929
Antonio <afranciulli@fi.uba.ar>
2024-06-12 18:21:54 -0300

    #62 - Modifico init.sh

--- init.sh
+++ init.sh
@@ -6,9 +6,9 @@ pip install pipenv
 mkdir .venv
 pipenv install flask
 pipenv install flask_sqlalchemy
-pip install requests
+pipenv install requests
 pipenv install mysql-connector-python
-pip3 install Flask-Cors
+pipenv install Flask-Cors
 export HOST="127.0.0.1"
 export FLASK_RUN_PORT=5050
 export FLASK_DEBUG=1
